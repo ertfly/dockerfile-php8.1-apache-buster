6cb387d109d0cba22c039991355961bcb478baf9
Eric Teixeira <ertfly@gmail.com>
2024-11-22 01:46:40 -0300

    fix

--- start.sh
+++ start.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 set -e
-
-cd /root
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
-. /root/.bashrc
-nvm install 18
-
-cd /front
-npm install
-npm run build
 cd /app
 apache2-foreground
